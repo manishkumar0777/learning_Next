cd190eb8d459453e508d3d53e4b88eb3dfeb74d8
manishkumar0777 <manishkumar970034@gmail.com>
2025-08-24 18:56:19 +0530

    nextjs

--- huxn_02/src/app/project3/page.tsx
+++ huxn_02/src/app/project3/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link"
 
 
-const Project2 = () => {
+const Project3 = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center p-8 bg-gradient-to-br from-gray-900 to-gray-800">
-      <h1 className="text-gray-200 text-9xl mb-3 font-extrabold">PROJECT 2</h1>
+      <h1 className="text-gray-200 text-9xl mb-3 font-extrabold">PROJECT 3</h1>
       <div className="p-2 px-6 rounded-lg bg-blue-600 shadow-lg hover:shadow-xl hover:bg-blue-700">
         <Link href='/project2/games'>Go to Project</Link>
       </div>
@@ -13,4 +13,4 @@ const Project2 = () => {
   )
 }
 
-export default Project2
+export default Project3
